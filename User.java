032681162df658be67b5a3fe4e21de75e900bc10
LauMeiJun <122443027+laumeijun@users.noreply.github.com>
2023-04-24 11:28:04 +0800

    Add files via upload
    Init

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+
+    String Name;
+    String Description;
+    int Id;
+    boolean Followed;
+
+    public User(String name, String description, int id, boolean followed) {
+        Name = name;
+        Description = description;
+        Id = id;
+        Followed = followed;
+
+}
