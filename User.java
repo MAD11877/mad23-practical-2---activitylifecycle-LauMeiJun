fba7177de59b9e0faf57a1df044bf79fb6041a40
MeiJun <s10241969@connect.np.edu.sg>
2023-05-08 10:55:41 +0800

    Add files via upload
    Init

--- User.java
+++ User.java
@@ -1,14 +1,36 @@
 public class User {
 
-    String Name;
-    String Description;
-    int Id;
-    boolean Followed;
+    private String Name;
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name)
+    {
+        this.Name = name;
+    }
 
-    public User(String name, String description, int id, boolean followed) {
-        Name = name;
-        Description = description;
-        Id = id;
-        Followed = followed;
-
+    private String Description;
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    private int Id;
+    public String getId() {
+        return Id;
+    }
+    public void setId(String id)
+    {
+        this.Id = id;
+    }
+    private boolean Followed;
+    public String getFollowed() {
+        return Followed;
+    }
+    public void setFollowed(String followed)
+    {
+        this.Followed = Followed;
+    }
 }
